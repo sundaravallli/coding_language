8745a9745548e3acdd6c8d47a9acbc4158c5c0c2
sundaravalli <sundaravalli1@gmail.com>
2019-10-10 13:12:28 -0700

    Update changes in coding_language

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,49 @@ $(document).ready(function() {
        $("#congrats").hide();
        };
   $(".clickable").click(function(){   
-      $("#survey_1").show();
-  });
-  $("#yes_rdaio").click(function(){   
-    $("#survey_2").show();
-});
-$("#high").click(function(){   
-    $("#survey_3").show();
-});
-$("#web").click(function(){   
-    $("#survey_4").show();
-});
-$("#design").click(function(){   
-    $("#survey_5").show();
-});
+      $("#survey_1").show(); 
+    });
+    var yes = document.getElementById("yes_rdaio");
+    var no = document.getElementById("no_rdaio");
+
+    yes.onclick = function(){
+        $("#survey_2").show();  
+    }
+    
+    no.onclick = function(){
+        alert("OOPS!! You have take one on one basic course");
+    }
+      
+    var High_level = document.getElementById("high");
+    var Low_level = document.getElementById("low");
+    High_level.onclick = function(){
+        $("#survey_3").show();  
+    }
+    
+    Low_level.onclick = function(){
+        alert("You have many options:Java, C++,C courses will start in a couple of weeks:");
+    }
+ 
+    var Web_Development = document.getElementById("web");
+    var Artificial_Intelligence = document.getElementById("AI");
+
+    Artificial_Intelligence.onclick = function(){
+        $("#survey_4").show();  
+    }
+    
+    Web_Development.onclick = function(){
+        prompt("Do you want to get email about this:");
+    }
+    var Design = document.getElementById("design");
+    var Back_end = document.getElementById("backend");
+
+    Design.onclick = function(){
+        confirm("You can pursue after completing backend");
+    }
+    Back_end.onclick = function(){
+        $("#survey_5").show();  
+    }
+
 $("#ui").click(function(){   
     alert('Hey... We think RUBY,PYTHON & HTML would be opt for you. Thanks....');
 });
