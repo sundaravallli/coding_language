38da2028e7bfdb0bdf6e60e2787646eb9ba7ac09
sundaravalli <53624127+sundaravallli@users.noreply.github.com>
2019-10-24 06:16:22 -0700

    Update scripts.js
    Modification in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,63 @@
-$(document).ready(function() {
-   document.getElementById("myFrame").onload = function() {
-       $("#survey_1").hide();
-       $("#survey_2").hide();
-       $("#survey_3").hide();
-       $("#survey_4").hide();
-       $("#survey_5").hide();
-       $("#congrats").hide();
-       };
-  $(".clickable").click(function(){   
-      $("#survey_1").show(); 
-    });
-    var yes = document.getElementById("yes_rdaio");
-    var no = document.getElementById("no_rdaio");
-ss
-    yes.onclick = function(){
-        $("#survey_2").show();  
-    }
-    
-    no.onclick = function(){
-        alert("OOPS!! You have take one on one basic course");
-    }
-      
-    var High_level = document.getElementById("high");
-    var Low_level = document.getElementById("low");
-    High_level.onclick = function(){
-        $("#survey_3").show();  
-    }
-    
-    Low_level.onclick = function(){
-        alert("You have many options:Java, C++,C courses will start in a couple of weeks:");
-    }
- 
-    var Web_Development = document.getElementById("web");
-    var Artificial_Intelligence = document.getElementById("AI");
-
-    Artificial_Intelligence.onclick = function(){
-        $("#survey_4").show();  
-    }
-    
-    Web_Development.onclick = function(){
-        prompt("Do you want to get email about this:");
-    }
-    var Design = document.getElementById("design");
-    var Back_end = document.getElementById("backend");
-
-    Design.onclick = function(){
-        confirm("You can pursue after completing backend");
-    }
-    Back_end.onclick = function(){
-        $("#survey_5").show();  
-    }
-
-$("#ui").click(function(){   
-    alert('Hey... We think RUBY,PYTHON & HTML would be opt for you. Thanks....');
-});
-
-  });
-
-  
+<!Doctype html>
+<html>
+    <head>
+       <link href="css/bootstrap.css" rel="stylesheet" type="text/css"></link>
+       <link href="css/styles.css" rel="stylesheet" type="text/css"></link>
+       <script src="js/jQuery.js"></script>
+       <script src="js/scripts.js"></script>
+       <title>Coding-Languages</title>
+    </head>
+    <body id="myFrame">
+            <h1 align="center">Epicodus Training Centre</h1>
+            <div> 
+                <div>
+                        <label id="name">Name</label>
+                        <input name="namefield"></input>
+                </div>        
+          
+            <br>
+            <div>
+                    <label>Age</label>
+                    <input id="ageField"></input>
+            </div>
+            
+            </div>
+            <p>Welcome to Epicodus!! If you are here to dive deeply in coding,this is a right place for you!!
+            <br>Have an amazing coding!! Here,you <button class="clickable">GO</button>
+            </p>
+                 <div id="survey_1">
+                     <label>Do you have any prior knowledge?</label><br>
+                     <input type="radio" name="knowledge" id="yes_rdaio" value="yes">yes
+                     <input type="radio" name="knowledge" id="no_rdaio" value="no">no
+                 </div>
+            <br>
+            <div id="survey_2">
+            <label>Which level language do you prefer to study?</label><br>
+            <input type="radio" name="language" id="high" value="High_level">High_level
+            <input type="radio" name="language" id="low" value="Low_level">Low_level
+        </div>
+            <br>
+            <div id="survey_3">
+            <label>What kind of field do you like to move in?</label><br>
+            <input type="radio" name="area" id="web" value="Web_Development">Web_Development
+            <input type="radio" name="area" id="AI" value="Artficial_Intelligence">Artficial_Intelligence
+        </div>
+            <br>
+            <div id="survey_4">
+            <label>What is your area of interest?</label><br>
+            <input type="radio" name="interest" id="design" value="Design">Design
+            <input type="radio" name="interest" id="backend" value="Back_end">Back_end 
+        </div>
+            <br>
+            <div id="survey_5">
+            <label>Which kind of design do you prefer to?</label><br>
+            <input type="radio" name="design" id="ui" value="UI">UI
+            <input type="radio" name="design" id="ux" value="UX">UX
+        </div>
+            <br>
+            <div id="congrats">
+            <p>Hey,Congratulations!!</button>Click here</button></p>
+        </div>
+        
+    </body>
+    </html>
